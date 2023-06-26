5bb39d598d7bae26ce22e3bbc9625130c83a99d0
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-26 14:39:04 +0200

    EDIT MS0 completata

--- js/my-script.js
+++ js/my-script.js
@@ -5,3 +5,44 @@
 // BONUS
 // 4. carico l' immagine come tale o usando innerHTML oppure (meglio) set attribute di src con template literal
 // 5. applico classi bootstrap agli elementi html e creo x card
+
+const teamList = [
+    {
+        name:"Wayne Barnett",
+        role:"Founder & CEO",
+        img: "img/wayne-barnett-founder-ceo.jpg"
+    },
+    
+    {
+        name:"Angela Carroll",
+        role:"Chief Editor",
+        img: "img/angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        name:"Walter Gordon",
+        role:"Office Manager",
+        img: "img/walter-gordon-office-manager.jpg"
+    },
+
+    {
+        name:"Angela Lopez",
+        role:"Social Media Manager",
+        img: "img/angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        name:"Scott Estrada",
+        role:"Developer",
+        img: "img/scott-estrada-developer.jpg"
+    },
+
+    {
+        name:"Barbara Ramos",
+        role:"Graphic Designer",
+        img: "img/barbara-ramos-graphic-designer.jpg"
+    }
+    
+        
+]
+
